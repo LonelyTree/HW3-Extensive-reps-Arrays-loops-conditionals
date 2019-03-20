0f6a4417112d3125808ac7359a5114a7d6cf8bdb
Alex Hughes <alexhugh121@gmail.com>
2019-03-20 16:41:08 -0700

    Commit 4 - Fizz Buzz answered

--- js/main.js
+++ js/main.js
@@ -50,3 +50,16 @@ for(let e = 1; e <= 20; e++){
     }   else {
     console.log(e, "Love me, pet me! HSSSSSS!");
 }}
+
+
+for(let i=1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if(i % 3 === 0) {
+        console.log("Fizz");
+    } else if(i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    };
+};
