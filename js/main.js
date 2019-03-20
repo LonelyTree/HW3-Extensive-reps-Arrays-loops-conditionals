a3910f2fe61ebdbf6a103eedc5c0764805dc5280
Alex Hughes <alexhugh121@gmail.com>
2019-03-20 16:02:00 -0700

    Commit 1 - Easy Going answered

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* A: A fork is used when collaboration is neccessary as it permits the user
+alter the origin master without actually modifying the true origin master. 
+After programmer is done with their version, they can push their changes to their 
+forked version of the origin master then submit a pull requst for true origin master. 
+*/
+
+for( let i= 1 ; i <= 20; i++){
+    console.log(i);
+}
