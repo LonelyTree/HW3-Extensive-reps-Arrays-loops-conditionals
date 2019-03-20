c9a022b8083a0f9f883078a40a2c63c3450c8c1c
Alex Hughes <alexhugh121@gmail.com>
2019-03-20 16:05:07 -0700

    Commit 2 - Get Even answered

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ forked version of the origin master then submit a pull requst for true origin ma
 
 for( let i= 1 ; i <= 20; i++){
     console.log(i);
-}
+};
+
+for( let i = 0; i <= 200; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    };
+};
